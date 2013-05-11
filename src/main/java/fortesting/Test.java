38a4a02982099346a958fbd756b428db8de4663b
Augie Millares <augie@augiemillares.com>
2013-05-11 12:11:37 -0400

    this is a test

---
+++ src/main/java/fortesting/Test.java
@@ -0,0 +1,15 @@
+package fortesting;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Augie
+ * Date: 5/11/13
+ * Time: 12:11 PM
+ * <p/>
+ * Modification:
+ * ----------------------------
+ */
+
+
+public class Test {
+}
